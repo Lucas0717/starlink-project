47dcd359550a3a47b9ad09e1930de5bb44048db1
Lucas0717 <gangyupa@usc.edu>
2023-03-04 14:20:53 -0500

    add

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "5PAJJQ-JJ9UAN-K5PRU4-4V8Z";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://54.219.224.247/80";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
